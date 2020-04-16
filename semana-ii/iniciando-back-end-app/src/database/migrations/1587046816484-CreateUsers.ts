17df335c7ed6c77a7b0c5a5aa08396f65327b7f9
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2020-04-16 13:01:10 -0300

    Module II: User Registration

--- semana-ii/iniciando-back-end-app/src/database/migrations/1587046816484-CreateUsers.ts
+++ semana-ii/iniciando-back-end-app/src/database/migrations/1587046816484-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1586969125937
-  implements MigrationInterface {
+export default class CreateUsers1587046816484 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,13 +14,18 @@ export default class CreateAppointments1586969125937
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
-            isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
             isNullable: false,
           },
           {
@@ -40,6 +44,6 @@ export default class CreateAppointments1586969125937
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
