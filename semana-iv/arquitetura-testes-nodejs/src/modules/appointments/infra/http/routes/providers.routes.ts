1d6e10503c5594701c8c4dce035d05ec804b120a
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2020-05-15 20:59:18 -0300

    Module V: Section VI, Class 8: Routes & Controllers

--- semana-iv/arquitetura-testes-nodejs/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ semana-iv/arquitetura-testes-nodejs/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,14 +1,26 @@
 import { Router } from 'express';
 
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController';
+import ProviderDayAvailabilitiesController from '@modules/appointments/infra/http/controllers/ProviderDayAvailabilitiesController';
+import ProviderMonthAvailabilitiesController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilitiesController';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providerDayAvailabilitiesController = new ProviderDayAvailabilitiesController();
+const providerMonthAvailabilitiesController = new ProviderMonthAvailabilitiesController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.show);
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providerDayAvailabilitiesController.index,
+);
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerMonthAvailabilitiesController.index,
+);
 
 export default providersRouter;
